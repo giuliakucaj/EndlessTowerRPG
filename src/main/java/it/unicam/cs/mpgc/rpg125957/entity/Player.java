aed8c482ee6866ce56ff93828a980f73ed088296
Giulia Kucaj <giulia.kucaj@studenti.unicam.it>
2026-06-07 10:13:03 +0200

    Add player class

--- src/main/java/it/unicam/cs/mpgc/rpg125957/entity/Player.java
+++ src/main/java/it/unicam/cs/mpgc/rpg125957/entity/Player.java
@@ -1,4 +1,14 @@
 package it.unicam.cs.mpgc.rpg125957.entity;
 
-public class Player {
+//Classe che rappresenta il giocatore
+public class Player extends Entity {
+
+    public Player(String name, Stats stats) {
+        super(name, stats);
+    }
+
+    //Metodo per attaccare un nemico
+    public void attack(Entity target) {
+        target.takeDamage(getAttackValue());
+    }
 }
