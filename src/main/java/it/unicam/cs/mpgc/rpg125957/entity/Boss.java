aed874ecb9c1715dfa89e45838b5dc8fdb967a19
Giulia Kucaj <giulia.kucaj@studenti.unicam.it>
2026-06-07 10:52:09 +0200

    Add boss entityy

--- src/main/java/it/unicam/cs/mpgc/rpg125957/entity/Boss.java
+++ src/main/java/it/unicam/cs/mpgc/rpg125957/entity/Boss.java
@@ -1,4 +1,17 @@
 package it.unicam.cs.mpgc.rpg125957.entity;
 
-public class Boss {
+//Classe che rappresenta il boss finale della torre
+public class Boss extends Enemy {
+
+    private final int floorNumber;
+
+    public Boss(String name, Stats stats, int rewardExperience, int rewardGold, int floorNumber) {
+        super(name, stats, rewardExperience, rewardGold);
+        this.floorNumber = floorNumber;
+    }
+
+    //Piano in cui appare il boss
+    public int getFloorNumber() {
+        return floorNumber;
+    }
 }
