7a8f12b4f85d400349bf636cabdebc9affdbd15d
Giulia Kucaj <giulia.kucaj@studenti.unicam.it>
2026-06-07 10:08:20 +0200

    Add abstract entity class

--- src/main/java/it/unicam/cs/mpgc/rpg125957/entity/Entity.java
+++ src/main/java/it/unicam/cs/mpgc/rpg125957/entity/Entity.java
@@ -1,4 +1,32 @@
 package it.unicam.cs.mpgc.rpg125957.entity;
 
-public class Entity {
+//Classe base astratta per tutti i personaggi del gioco
+public abstract class Entity {
+
+    private final String name;
+    private final Stats stats;
+
+    public Entity(String name, Stats stats) {
+        this.name = name;
+        this.stats = stats;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public Stats getStats() {
+        return stats;
+    }
+
+    public boolean isAlive() {
+        return stats.isAlive();
+    }
+
+    public void takeDamage(int damage) {
+        stats.takeDamage(damage);
+    }
+
+    public int getAttackValue() {
+        return stats.getAttack();
+    }
 }
