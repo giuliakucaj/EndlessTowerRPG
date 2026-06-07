1827b6cd5360ee21abeaef0f660ffa8330d62864
Giulia Kucaj <giulia.kucaj@studenti.unicam.it>
2026-06-07 10:38:49 +0200

    Add enemy

--- src/main/java/it/unicam/cs/mpgc/rpg125957/entity/Enemy.java
+++ src/main/java/it/unicam/cs/mpgc/rpg125957/entity/Enemy.java
@@ -1,4 +1,29 @@
 package it.unicam.cs.mpgc.rpg125957.entity;
 
-public class Enemy {
+//Classe che rappresenta un nemico
+public class Enemy extends Entity {
+
+    private final int rewardExperience;
+    private final int rewardGold;
+
+    public Enemy(String name, Stats stats, int rewardExperience, int rewardGold) {
+        super(name, stats);
+        this.rewardExperience = rewardExperience;
+        this.rewardGold = rewardGold;
+    }
+
+    //Esperienza ottenuta sconfiggendo il nemico
+    public int getRewardExperience() {
+        return rewardExperience;
+    }
+
+    //Oro ottenuto sconfiggendo il nemico
+    public int getRewardGold() {
+        return rewardGold;
+    }
+
+    //Metodo per attaccare il giocatore
+    public void attack(Entity target) {
+        target.takeDamage(getAttackValue());
+    }
 }
